4445d0a62e79664d8a6cbc418a9434d2987d83d3
Glo <glo.movil@gmail.com>
2023-07-13 23:05:26 +0200

    update map dynamically centered

--- src/pages/LocalsDatosMap.js
+++ src/pages/LocalsDatosMap.js
@@ -8,13 +8,20 @@ import { Marker, Popup } from 'react-leaflet';
 import { datosBarriosPosition } from '../data/datosBarriosPosition';
 import { DivMapa } from './LocalsDatosIndice.styled';
 
+function MyChildMapComponent({ pos }) {
+    const map = useMap()
+    //console.log('map center:', map.getCenter())
+    map.setView(pos, map.getZoom());
+    return null;
+}
+
 const LocalsDatosMap = () => {
     //idBarri has to be greater than 0 to initially show a neigbourhood with score on the map
     const [idBarri, setIdBarri] = useState(10);
     const [position, setPosition] = useState([41.39, 2.17]);
     const [nameBarri, setNameBarri] = useState("");
     const [indexProx, setIndexProx] = useState("");
-    const [newCenter, setNewCenter] = useState([41.39, 2.17]);
+    // const [newCenter, setNewCenter] = useState([41.39, 2.17]);
 
     useEffect(() => {
         const barriSelectedUniArr = datosBarriosPosition.filter(barri => Number(barri.id) === Number(idBarri));
@@ -27,9 +34,6 @@ const LocalsDatosMap = () => {
 
     }, [idBarri]);
 
-    useEffect(() => {
-        setNewCenter(position);
-    }, [position]);
 
     const handleChangeSelect = (e) => {
         setIdBarri(e.target.value);
@@ -44,9 +48,10 @@ const LocalsDatosMap = () => {
                     datosBarriosPosition.sort((a, b) => a.name > b.name ? 1 : -1).map((barrio) => <option key={barrio.id} value={(barrio.id)} >{barrio.name} </option>)
                 }
             </select >
-            <MapContainer center={newCenter} zoom={12} scrollWheelZoom={false} className="map">
+            <MapContainer center={position} zoom={14} scrollWheelZoom={false} className="map">
+                <MyChildMapComponent pos={position} />
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="https://opendata-ajuntament.barcelona.cat/data/ca/dataset/fonts/resource/6465668a-6b7a-456e-ade0-1101d772966f">Open Data BCN</a>'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 <Marker position={position}>
@@ -55,8 +60,6 @@ const LocalsDatosMap = () => {
                     </Popup>
                 </Marker>
             </MapContainer>
-
-
         </DivMapa>
     )
 }
